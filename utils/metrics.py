b9dcf48f729101ca91de0165f08d9c8d2479153b
Zmw2006 <1771373619@qq.com>
2026-09-24 21:25:50 +0800

    Add reusable evaluation utilities

---
+++ utils/metrics.py
@@ -0,0 +1,11 @@
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+
+def evaluate(y_true, y_pred):
+    return {
+        'accuracy': accuracy_score(y_true, y_pred),
+        'precision': precision_score(y_true, y_pred, average='macro'),
+        'recall': recall_score(y_true, y_pred, average='macro'),
+        'f1': f1_score(y_true, y_pred, average='macro')
+    }

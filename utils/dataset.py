bebfe82f0c95a96e48cab3fa75fdb0f7497b3796
Zmw2006 <1771373619@qq.com>
2026-09-24 20:46:30 +0800

    Add dataset loader

---
+++ utils/dataset.py
@@ -0,0 +1,8 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_data(batch=64):
+    tf=transforms.Compose([transforms.ToTensor()])
+    train=datasets.MNIST('./data',train=True,download=True,transform=tf)
+    test=datasets.MNIST('./data',train=False,download=True,transform=tf)
+    return DataLoader(train,batch_size=batch,shuffle=True),DataLoader(test,batch_size=batch)
